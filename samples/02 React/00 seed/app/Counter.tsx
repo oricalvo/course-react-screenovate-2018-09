ec1fcef7bf8df33e752fd1cf7114849d8b89106e
oricalvo <ori.calvo@gmail.com>
2018-10-03 14:58:40 +0300

    more

---
+++ samples/02 React/00 seed/app/Counter.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="App">
+            <h1>Hello React</h1>
+        </div>
+    }
+}
+
